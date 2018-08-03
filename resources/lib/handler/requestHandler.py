d612fed76759d79e4c0c9a248d2768453190c554
anis3 <anis3.n@web.de>
2018-08-03 02:21:24 +0000

    go to urllib2

--- resources/lib/handler/requestHandler.py
+++ resources/lib/handler/requestHandler.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python2.7
-import hashlib
-import httplib
-import os
-import socket
-import sys
-import time
-import urllib
-
-import mechanize
-import xbmcgui
-
-from resources.lib import common
-from resources.lib import logger, cookie_helper
+import cookielib, hashlib, httplib, os, socket, sys, time, urllib, urllib2, xbmcgui
+from resources.lib import logger, cookie_helper, common
 from resources.lib.cBFScrape import cBFScrape
 from resources.lib.cCFScrape import cCFScrape
 from resources.lib.config import cConfig
 
-
 class cRequestHandler:
-    def __init__(self, sUrl, caching=True, ignoreErrors=False, compression=True, formIndex=0):
+    def __init__(self, sUrl, caching=True, ignoreErrors=False, compression=True):
         self.__sUrl = sUrl
         self.__sRealUrl = ''
         self.__cType = 0
@@ -26,7 +14,6 @@ class cRequestHandler:
         self.__aResponses = {}
         self.__headerEntries = {}
         self.__cachePath = ''
-        self.__formIndex = formIndex
         self._cookiePath = ''
         self.ignoreDiscard(False)
         self.ignoreExpired(False)
@@ -41,9 +28,12 @@ class cRequestHandler:
         self.setCachePath()
         self.__setCookiePath()
         self.__sResponseHeader = ''
-
         if self.requestTimeout >= 60 or self.requestTimeout <= 10:
             self.requestTimeout = 60
+
+    def set_data(self, data):
+        ' Set the data (a bytestring) to be sent with this request '
+        self.data = data
 
     def removeNewLines(self, bRemoveNewLines):
         self.__bRemoveNewLines = bRemoveNewLines
@@ -69,9 +59,6 @@ class cRequestHandler:
 
     def addResponse(self, key, value):
         self.__aResponses[key] = value
-
-    def setFormIndex(self, index):
-        self.__formIndex = index
 
     def getResponseHeader(self):
         return self.__sResponseHeader
@@ -100,35 +87,32 @@ class cRequestHandler:
             if sContent:
                 return sContent
 
-        cookieJar = mechanize.LWPCookieJar(filename=self._cookiePath)
+        cookieJar = cookielib.LWPCookieJar(filename=self._cookiePath)
         try:  # TODO ohne try evtl.
             cookieJar.load(ignore_discard=self.__bIgnoreDiscard, ignore_expires=self.__bIgnoreExpired)
         except Exception as e:
             logger.info(e)
 
         sParameters = urllib.urlencode(self.__aParameters, True)
+        handlers = [urllib2.HTTPHandler(), urllib2.HTTPSHandler(), urllib2.HTTPCookieProcessor(cookiejar=cookieJar)]
 
-        handlers = [mechanize.HTTPCookieProcessor(cookiejar=cookieJar),
-                    mechanize.HTTPEquivProcessor,
-                    mechanize.HTTPRefreshProcessor]
-        if sys.version_info >= (2, 7, 9) and sys.version_info < (2, 7, 11):
+        if (2, 7, 9) <= sys.version_info < (2, 7, 11):
             handlers.append(newHTTPSHandler)
-        opener = mechanize.build_opener(*handlers)
+        opener = urllib2.build_opener(*handlers)
         if (len(sParameters) > 0):
-            oRequest = mechanize.Request(self.__sUrl, sParameters)
+            oRequest = urllib2.Request(self.__sUrl, sParameters)
         else:
-            oRequest = mechanize.Request(self.__sUrl)
+            oRequest = urllib2.Request(self.__sUrl)
 
         for key, value in self.__headerEntries.items():
             oRequest.add_header(key, value)
         cookieJar.add_cookie_header(oRequest)
-
         user_agent = self.__headerEntries.get('User-Agent', common.FF_USER_AGENT)
 
         try:
             oResponse = opener.open(oRequest, timeout=self.requestTimeout)
-        except mechanize.HTTPError, e:
-            if e.code == 503 and e.headers.get("Server") == 'cloudflare-nginx' or 'cloudflare':
+        except urllib2.HTTPError, e:
+            if e.code == 503 and e.headers.get("Server") == 'cloudflare':
                 html = e.read()
                 oResponse = self.__check_protection(html, user_agent, cookieJar)
                 if not oResponse:
@@ -140,10 +124,10 @@ class cRequestHandler:
                 return ''
             else:
                 oResponse = e
-        except mechanize.URLError, e:
+        except urllib2.URLError, e:
             if not self.ignoreErrors:
                 if hasattr(e.reason, 'args') and e.reason.args[0] == 1 and sys.version_info < (2, 7, 9):
-                    xbmcgui.Dialog().ok('xStream', str(e.reason), '','For this request is Python v2.7.9 or higher required.')
+                    xbmcgui.Dialog().ok('xStream', str(e.reason), '', 'For this request is Python v2.7.9 or higher required.')
                 else:
                     xbmcgui.Dialog().ok('xStream', str(e.reason))
             logger.error("URLError " + str(e.reason) + " Url: " + self.__sUrl)
@@ -159,36 +143,16 @@ class cRequestHandler:
         if self.__sResponseHeader.get('Content-Encoding') == 'gzip':
             import gzip
             import StringIO
-            data = StringIO.StringIO(oResponse.read())
-            gzipper = gzip.GzipFile(fileobj=data, mode='rb')
-            try:
-                oResponse.set_data(gzipper.read())
-            except:
-                oResponse.set_data(gzipper.extrabuf)
-        
-        if self.__aResponses:
-            forms = mechanize.ParseResponse(oResponse, backwards_compat=False)
-            form = forms[self.__formIndex]
-            for field in self.__aResponses:
-                #logger.info("Field: " + str(not field in form))
-                try: form.find_control(name=field)
-                except:
-                    form.new_control("text", field, {"value":""})
-                    form.fixup()
-                form[field] = self.__aResponses[field]
-            o = mechanize.build_opener(mechanize.HTTPCookieProcessor(cookieJar))
-            oResponse = o.open(form.click(), timeout=self.requestTimeout)
-
-        sContent = oResponse.read()
+            sContent = gzip.GzipFile(fileobj=StringIO.StringIO(oResponse.read())).read()
+        else:
+            sContent = oResponse.read()
 
         checked_response = self.__check_protection(sContent, user_agent, cookieJar)
         if checked_response:
             oResponse = checked_response
             sContent = oResponse.read()
-
         cookie_helper.check_cookies(cookieJar)
         cookieJar.save(ignore_discard=self.__bIgnoreDiscard, ignore_expires=self.__bIgnoreExpired)
-
 
         if (self.__bRemoveNewLines == True):
             sContent = sContent.replace("\n", "")
@@ -196,27 +160,23 @@ class cRequestHandler:
 
         if (self.__bRemoveBreakLines == True):
             sContent = sContent.replace("&nbsp;", "")
-
         self.__sRealUrl = oResponse.geturl()
 
         oResponse.close()
         if self.caching and self.cacheTime > 0:
             self.writeCache(self.getRequestUri(), sContent)
-
         return sContent
 
     def __check_protection(self, html, user_agent, cookie_jar):
         oResponse = None
-
         if 'cf-browser-verification' in html:
             oResponse = cCFScrape().resolve(self.__sUrl, cookie_jar, user_agent)
-        elif 'Blazingfast.io' in html:
+        elif 'Blazingfast.io' in html or 'xhr.open("GET","' in html:
             oResponse = cBFScrape().resolve(self.__sUrl, cookie_jar, user_agent)
-
         return oResponse
 
     def getHeaderLocationUrl(self):
-        opened = mechanize.urlopen(self.__sUrl)
+        opened = urllib2.urlopen(self.__sUrl)
         return opened.geturl()
 
     def __setCookiePath(self):
@@ -228,7 +188,7 @@ class cRequestHandler:
         self._cookiePath = cookieFile
 
     def getCookie(self, sCookieName, sDomain=''):
-        cookieJar = mechanize.LWPCookieJar()
+        cookieJar = cookielib.LWPCookieJar()
         try:  # TODO ohne try evtl.
             cookieJar.load(self._cookiePath, self.__bIgnoreDiscard, self.__bIgnoreExpired)
         except Exception as e:
@@ -240,18 +200,15 @@ class cRequestHandler:
                     return entry
                 elif entry.domain == sDomain:
                     return entry
-
         return False
 
     def setCookie(self, oCookie):
-        cookieJar = mechanize.LWPCookieJar()
+        cookieJar = cookielib.LWPCookieJar()
         try:  # TODO ohne try evtl.
             cookieJar.load(self._cookiePath, self.__bIgnoreDiscard, self.__bIgnoreExpired)
         except Exception as e:
             logger.info(e)
-
         cookieJar.set_cookie(oCookie)
-
         cookieJar.save(self._cookiePath, self.__bIgnoreDiscard, self.__bIgnoreExpired)
 
     def ignoreDiscard(self, bIgnoreDiscard):
@@ -273,7 +230,7 @@ class cRequestHandler:
         h = hashlib.md5(url).hexdigest()
         cacheFile = os.path.join(self.__cachePath, h)
         fileAge = self.getFileAge(cacheFile)
-        if fileAge > 0 and fileAge < self.cacheTime:
+        if 0 < fileAge < self.cacheTime:
             try:
                 fhdl = file(cacheFile, 'r')
                 content = fhdl.read()
@@ -293,7 +250,8 @@ class cRequestHandler:
         except:
             logger.info('Could not write Cache')
 
-    def getFileAge(self, cacheFile):
+    @staticmethod
+    def getFileAge(cacheFile):
         try:
             fileAge = time.time() - os.stat(cacheFile).st_mtime
         except:
@@ -308,18 +266,15 @@ class cRequestHandler:
             if fileAge > self.cacheTime:
                 os.remove(cacheFile)
 
-
 # python 2.7.9 and 2.7.10 certificate workaround
-class newHTTPSHandler(mechanize.HTTPSHandler):
-    def do_open(self, conn_factory, req):
+class newHTTPSHandler(urllib2.HTTPSHandler):
+    def do_open(self, conn_factory, req, **kwargs):
         conn_factory = newHTTPSConnection
-        return mechanize.HTTPSHandler.do_open(self, conn_factory, req)
+        return urllib2.HTTPSHandler.do_open(self, conn_factory, req)
 
 
 class newHTTPSConnection(httplib.HTTPSConnection):
-    def __init__(self, host, port=None, key_file=None, cert_file=None, strict=None,
-                 timeout=socket._GLOBAL_DEFAULT_TIMEOUT, source_address=None, context=None):
+    def __init__(self, host, port=None, key_file=None, cert_file=None, strict=None, timeout=socket._GLOBAL_DEFAULT_TIMEOUT, source_address=None, context=None):
         import ssl
         context = ssl._create_unverified_context()
-        httplib.HTTPSConnection.__init__(self, host, port, key_file, cert_file, strict, timeout, source_address,
-                                         context)
+        httplib.HTTPSConnection.__init__(self, host, port, key_file, cert_file, strict, timeout, source_address, context)
