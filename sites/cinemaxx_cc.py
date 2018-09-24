5e618b758d42f122792ae8d77519e55e0a024888
anis3 <anis3.n@web.de>
2018-09-24 20:51:56 +0000

    [cinemaxx.cc] Fix Hoster

--- sites/cinemaxx_cc.py
+++ sites/cinemaxx_cc.py
@@ -208,8 +208,9 @@ def showHosters():
 
 
 def getHosterUrl(sUrl=False):
-    ref = ParameterHandler().getValue('entryUrl')
-    return [{'streamUrl': sUrl + '|Referer=' + ref + '&User-Agent=Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0' , 'resolved': True}]
+    if sUrl.startswith('//'):
+        sUrl = 'https:' + sUrl
+    return [{'streamUrl': sUrl + '|Referer=' + sUrl + '&User-Agent=Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0', 'resolved': True}]
 
 
 def showSearch():
